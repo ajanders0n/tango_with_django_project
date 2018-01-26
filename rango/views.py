6ceb3f044cd45fc2e8790e0ec32bcc56789e5f3b
Alexander Anderson <2079103a@student.gla.ac.uk>
2018-01-26 10:31:41 +0000

    4.1 done

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+
+    # Construct a dictionary to pass to the template engine as its context.
+    context_dict = { 'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    # Returns the rendered response to send to the client.
+    return render(request, 'rango/index.html', context=context_dict)
